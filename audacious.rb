37c36e4ca36fa926fe68bceb91c0520b5b980d69
neofright <68615872+neofright@users.noreply.github.com>
2022-02-14 14:02:16 +0000

    Create audacious.rb

---
+++ audacious.rb
@@ -0,0 +1,107 @@
+class Audacious < Formula
+  desc "Free and advanced audio player based on GTK+"
+  homepage "https://audacious-media-player.org/"
+  license "BSD-2-Clause"
+  revision 2
+
+  stable do
+    url "https://distfiles.audacious-media-player.org/audacious-4.2-beta1.tar.bz2"
+    sha256 "f19202ef2855ae8fe7bf3fcf071c13055b710d28971caccfab7d9598874fdff4"
+
+    resource "plugins" do
+      url "https://distfiles.audacious-media-player.org/audacious-plugins-4.2-beta1.tar.bz2"
+      sha256 "36e9df3462ab1db819263ca5f443bc4932ff99aba9e752ccba546c44cff2406f"
+    end
+  end
+
+  livecheck do
+    url "https://audacious-media-player.org/download"
+    regex(/href=.*?audacious[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_big_sur: "819afa4489c4f26017ce6966291d3b82b859ceb96476edf963973d4b75c010a8"
+    sha256 big_sur:       "8b29082bd4c5dd088db4d990b1c965f80022037e7b8f4cd5abcf0fbd111211fe"
+    sha256 catalina:      "7f7bea293e9f75013b2dd6dbf376644578a35165f2cb7e84a2c2634971a3bf9c"
+    sha256 x86_64_linux:  "a5883bc3e84402141c2e0effac40850fd8ca91f71ef33ac3f4140741e1349f1e"
+  end
+
+  head do
+    url "https://github.com/audacious-media-player/audacious.git"
+
+    resource "plugins" do
+      url "https://github.com/audacious-media-player/audacious-plugins.git"
+    end
+  end
+
+  depends_on "gettext" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "faad2"
+  depends_on "ffmpeg@4"
+  depends_on "flac"
+  depends_on "fluid-synth"
+  depends_on "glib"
+  depends_on "lame"
+  depends_on "libbs2b"
+  depends_on "libcue"
+  depends_on "libmodplug"
+  depends_on "libnotify"
+  depends_on "libopenmpt"
+  depends_on "libsamplerate"
+  depends_on "libsoxr"
+  depends_on "libvorbis"
+  depends_on "mpg123"
+  depends_on "neon"
+  depends_on "qt@5"
+  depends_on "sdl2"
+  depends_on "wavpack"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    args = std_meson_args + %w[
+      -Dgtk=false
+      -Dqt=true
+    ]
+
+    mkdir "build" do
+      system "meson", *args, "-Ddbus=false", ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+
+    resource("plugins").stage do
+      args += %w[
+        -Dcoreaudio=false
+        -Dmpris2=false
+        -Dmac-media-keys=true
+        -Dcpp_std=c++14
+      ]
+
+      ENV.prepend_path "PKG_CONFIG_PATH", lib/"pkgconfig"
+      mkdir "build" do
+        system "meson", *args, ".."
+        system "ninja", "-v"
+        system "ninja", "install", "-v"
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      audtool does not work due to a broken dbus implementation on macOS, so it is not built.
+      Core Audio output has been disabled as it does not work (fails to set audio unit input property).
+      GTK+ GUI is not built by default as the Qt GUI has better integration with macOS, and the GTK GUI would take precedence if present.
+    EOS
+  end
+
+  test do
+    system bin/"audacious", "--help"
+  end
+end
